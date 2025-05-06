332f8bdc89e4ae5ee6518772143381570d2f6335
JaxonC-06 <jaxoncostain@outlook.com>
2025-05-06 16:17:37 -0600

    Everything but stretch

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires ++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   /** Stretch **/
@@ -36,7 +49,13 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    if (this.creator === vampire.creator) {
+      return this.creator;
+    } else if (this.creator === vampire) {
+      return vampire;
+    } else if (vampire.creator = this) {
+      return this;
+    }
   }
 }
 
